07da9633376b12784ed6da8de5141aa510a80f87
Umer Masood Alvi <162821251+ualvi27@users.noreply.github.com>
2024-03-21 16:18:21 +0500

    Add files via upload

---
+++ B14Assignment 01.py
@@ -0,0 +1,24 @@
+# # Largest Num Program
+# num1 = int(input('Enter 1st num here'))
+# num2 = int(input('Enter 2nd num here'))
+# num3 = int(input('Enter 3rd num here'))
+# if (num1 > num2) and (num1 > num3):
+#     print(num1, " is Greatest number")
+# elif(num2 > num1) and (num2 > num3):
+#     print(num2,"is Greatest number ")
+# else:
+#     print('num3 is Greatest number')
+
+# Leap Year Program
+# year = int(input('enter a year'))
+#
+# if (year % 400 == 0) and  (year % 100 ==0):
+#     print (year, "is a leap year")
+# elif (year % 4==0) and (year % 100 !=0):
+#     print (year, "is a leap year")
+#
+# else:
+#     print (year, "is not leap year")
+
+# Count the vowel
+
